cf4e182d6bb55e2ad6587bcfc2c0e64da4b9abeb
arcticsnow <sfilhol@gmail.com>
2021-09-06 19:25:00 +0200

    started classification functions

--- projimdem/classify.py
+++ projimdem/classify.py
@@ -1,7 +1,9 @@
 '''
 Set of functions to classify pixels in images in 2 or more categories. Original intent is the classify snow vs. no snow area.
 
-Technique 1, Manual thresholding
+Technique 0, Manual thresholding
+
+Technique 1, MultuOsu (scikit-image)
 
 Technique 2, Salzano et al (2019)
 
@@ -10,4 +12,52 @@ Technique 3, PCA
 Technique 4, Clustering
 
 '''
+
+import matplotlib.pyplot as plt
+
+from skimage.color import rgb2hsv
+from skimage.filters import threshold_multiotsu
+
+#======================================
+# Technique 0, Manual Thresholding
+
+def classify_threshold(img_rgb, threshold, mask=None):
+    img_value = rgb2hsv(img_rgb)[:, :, 2]
+    
+    regions = img_value < threshold
+    if mask is not None:
+        regions = regions * mask
+    return regions
+
+def classify_blue_threshold(img_rgb, threshold, mask=None):
+        
+    regions = img_rgb[:,:,1] < threshold
+    if mask is not None:
+        regions = regions * mask
+    return regions
+    
+#======================================
+# Technique 1, MultiOtsu Thresholding
+#
+# reference: https://scikit-image.org/docs/stable/api/skimage.filters.html#skimage.filters.threshold_multiotsu
+
+def classify_multiostu(img_rgb, nb_classes=2, mask=None):
+    thresholds = threshold_multiotsu(img_rgb, classes=nb_classes)
+    regions = np.digitize(img_rgb, bins=thresholds)
+    
+    if mask is not None:
+        regions = regions * mask
+    
+    return regions
+
+
+#======================================
+# Technique 2, Salzano et al (2019)
+#
+# reference: https://www.mdpi.com/2076-3263/9/2/97/htm#B32-geosciences-09-00097
+
+def classify_snow_salzano(img_rgb):
+    # See with Kris to understand method detailed in Salzano et al. 2019 (https://www.mdpi.com/2076-3263/9/2/97/htm#B32-geosciences-09-00097)
+    
+
 
